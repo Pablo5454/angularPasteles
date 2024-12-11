985d803de940b6d40daaaebc3910d105736e3937
2AW3 User 17 <2AW3-17@yourdomain.com>
2024-12-11 12:25:07 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import listaDePostres from 'src/assets/json/postres.json';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  Postres: any = listaDePostres;
+  title = 'pasteles';
+}
